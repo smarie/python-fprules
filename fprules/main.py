52996a335a5403a674cc200c056ccd3f5456a393
Sylvain MARIE <sylvain.marie@se.com>
2019-08-01 15:37:45 +0200

    Added a `name` attribute in `FileItem`. Fixes #2 Improved `__repr__` of `FileItem`. Fixes #3 Simplified `gen_matching_files` so that it now always returns a tuple with two elements. This simplified `file_pattern` too. `file_pattern` now returns a generator. Fixes #1

--- fprules/main.py
+++ fprules/main.py
@@ -5,6 +5,11 @@ try:
 except ImportError:
     from pathlib2 import Path, PurePath
 
+try:
+    from typing import Union, Type, Any
+except ImportError:
+    pass
+
 
 def gen_matching_files(src_pattern,  # type: Path
                        ):
@@ -12,23 +17,18 @@ def gen_matching_files(src_pattern,  # type: Path
     Utility generator function used by `file_pattern` to yield of matching file
     elements corresponding to the provided `src_pattern`.
 
-    If `src_pattern` contains a double wildcard `**`, each element returned by
-    this generator list will be a tuple containing two elements:
+    Each element returned by this generator will be a tuple containing two elements:
 
      - the first element is the `Path` of the file or folder that matched the
        search
      - the second element is a string representing the part of the path
-       captured by the double wildcard. Otherwise the second element is `None`.
-
-    If no double wildcard `**` is present in `src_pattern`, only path elements
-    are yielded.
+       captured by the double wildcard if `**` was present in `src_pattern`.
+       Otherwise the second element is `None`.
 
     :param src_pattern: a `Path` representing the source pattern to match.
         The list returned will contain one item for each file matching
         this pattern, using `glob` to perform the match.
-    :return: a generator yielding either <file_path> items, or tuples
-        (<file_path>, <captured_double_wildcard_path>) depending on whether a
-        `**` was present in the pattern or not.
+    :return: a generator yielding tuples (<file_path>, <captured_double_wildcard_path>)
     """
     # -- validate the source pattern
     src_double_wildcard = None
@@ -63,7 +63,7 @@ def gen_matching_files(src_pattern,  # type: Path
     if src_double_wildcard is None:
         # simply yield the matching file Path items
         for matched_file in glob_results:
-            yield matched_file
+            yield matched_file, None
     else:
         # for each matching item, find the path captured by the '**' and yield
         for matched_file in glob_results:
@@ -91,7 +91,7 @@ def gen_matching_files(src_pattern,  # type: Path
 
 
 class FileItem(namedtuple('FileItem',
-                          ('src_path', 'has_multi_targets', 'dst_path'))):
+                          ('name', 'src_path', 'has_multi_targets', 'dst_path'))):
     """
     Represents an item created by `file_pattern(...)`.
     """
@@ -115,11 +115,39 @@ class FileItem(namedtuple('FileItem',
         else:
             secnd_str = self.dst_path.as_posix()
 
-        return "%s -> %s" % (self.src_path.as_posix(), secnd_str)
+        return "[%s] %s -> %s" % (self.name.as_posix(), self.src_path.as_posix(), secnd_str)
+
+    def __repr__(self):
+        # default to readable representation
+        return str(self)
 
 
-def file_pattern(src_pattern,  # type: Union[str, Any]
-                 dst_pattern   # type: Union[str, Any]
+def _validate_dst_pattern(dst_pattern, src_pattern, src_has_double_wildcard, pattern_name='Destination'):
+    """return a validated dst pattern string"""
+
+    # convert the dest pattern
+    if not isinstance(dst_pattern, str):
+        dst_pattern = str(dst_pattern)
+
+    # validate the dest pattern
+    if '*' in dst_pattern:
+        raise ValueError("%s pattern can not contain star '*' "
+                         "wildcards, only '%%' characters. Found '%s'"
+                         % (pattern_name, dst_pattern))
+    if '%%' in dst_pattern and not src_has_double_wildcard:
+        raise ValueError(
+            "%s pattern '%s' uses a folder path '%%%%' but source"
+            " pattern does not include any double-wildcard: '%s'"
+            % (pattern_name, dst_pattern, src_pattern))
+    return dst_pattern
+
+
+def file_pattern(src_pattern,          # type: Union[str, Any]
+                 dst_pattern,          # type: Union[str, Any]
+                 *,
+                 names=None,            # type: Union[str, Any]
+                 # src_attr='src_path',  # type: str
+                 # dst_attr='dst_path'   # type: str
                  ):
     # type: (...) -> List[FileItem]
     """
@@ -167,6 +195,11 @@ def file_pattern(src_pattern,  # type: Union[str, Any]
        by the `**` in the source pattern. In that case the source pattern MUST
        contain a double-wildcard, and only one.
 
+    Naming pattern `names` represents the friendly name of the items to create.
+    A value of `None` (default) will either fallback to `%%/%` or to `%`
+    depending on whether `src_pattern` contains a double wildcard or not. That
+    way, the default value should always be unique across the returned set.
+
     It is possible to declare multiple destination patterns by passing a `dict`
     `dst_pattern` instead of a single element. In that case the resulting list
     will contain `FileItem` instances that have one attribute per pattern.
@@ -180,6 +213,9 @@ def file_pattern(src_pattern,  # type: Union[str, Any]
     :param dst_pattern: a string or object representing the destination
         pattern to use to create target file paths. A dictionary can also be
         provided to create several target file paths at once
+    :param names: a string or object representing the naming pattern to use. A
+        value of `None` (default) provides a default pattern trying to
+        guarantee uniqueness while preserving compacity.
     :return: a list of `FileItem` instances with at least two fields `src_path`
         and `dst_path`. When `dst_pattern` is a dictionary, the items will also
         show one attribute per key in that dictionary.
@@ -194,84 +230,56 @@ def file_pattern(src_pattern,  # type: Union[str, Any]
     else:
         src_has_double_wildcard = '**' in str(src_pattern)
 
+    # default names pattern
+    if names is None:
+        names = "%%/%" if src_has_double_wildcard else "%"
+
     # create the generator
     match_generator = gen_matching_files(src_pattern)
 
-    # -- validate the destination patterns
-    def _validate_dst_pattern(dst_pattern):
-        """return a validated dst pattern string"""
-
-        # convert the dest pattern
-        if not isinstance(dst_pattern, str):
-            dst_pattern = str(dst_pattern)
-
-        # validate the dest pattern
-        if '*' in dst_pattern:
-            raise ValueError("Destination pattern can not contain star '*' "
-                             "wildcards, only '%%' characters. Found '%s'"
-                             % dst_pattern)
-        if '%%' in dst_pattern and not src_has_double_wildcard:
-            raise ValueError(
-                "Destination pattern '%s' uses a folder path '%%%%' but source"
-                " pattern does not include any double-wildcard: '%s'"
-                % (dst_pattern, src_pattern))
-        return dst_pattern
-
+    # -- validate all destination patterns
     try:
         # assume a dictionary of destination patterns
         for dst_name, _dst_pattern in dst_pattern.items():
-            dst_pattern[dst_name] = _validate_dst_pattern(_dst_pattern)
+            dst_pattern[dst_name] = \
+                _validate_dst_pattern(_dst_pattern, src_pattern,
+                                      src_has_double_wildcard)
         has_multi_targets = True
     except AttributeError:
         # single pattern
-        dst_pattern = _validate_dst_pattern(dst_pattern)
+        dst_pattern = _validate_dst_pattern(dst_pattern, src_pattern,
+                                            src_has_double_wildcard)
         has_multi_targets = False
 
-    res = []
-    if not src_has_double_wildcard:
-        def _create_dst_path(matching_file, dst_pattern):
-            # replace % with stem and convert to Path object
-            return Path(dst_pattern.replace('%', matching_file.stem))
+    # -- validate name pattern
+    names = _validate_dst_pattern(names, src_pattern,
+                                  src_has_double_wildcard, pattern_name='Name')
 
-        for f_path in match_generator:
-            # create the destination path(s)
-            if has_multi_targets:
-                # use an OrderedDict for legacy python compatibility
-                _items = [(dst_name, _create_dst_path(f_path, _dst_pattern))
-                          for dst_name, _dst_pattern in dst_pattern.items()]
-                dst_paths = OrderedDict(_items)
-            else:
-                dst_paths = _create_dst_path(f_path, dst_pattern)
+    def _create_dst_path(matching_file, captured_subpath, dst_pattern):
+        # replace %% with the **-captured sub-path
+        if captured_subpath is not None and '%%' in dst_pattern:
+            dst_path = dst_pattern.replace('%%', captured_subpath)
+        else:
+            dst_path = dst_pattern
 
-            # finally create the container object and append
-            item = FileItem(src_path=f_path, dst_path=dst_paths,
-                            has_multi_targets=has_multi_targets)
-            res.append(item)
-    else:
-        def _create_dst_path(matching_file, captured_subpath, dst_pattern):
-            # replace %% with the **-captured sub-path
-            if '%%' in dst_pattern:
-                dst_path = dst_pattern.replace('%%', captured_subpath)
-            else:
-                dst_path = dst_pattern
+        # replace % with file stem and convert to Path object
+        return Path(dst_path.replace('%', matching_file.stem))
 
-            # replace % with file stem and convert to Path object
-            return Path(dst_path.replace('%', matching_file.stem))
+    for f_path, capt_subpath in match_generator:
+        # create the destination path(s)
+        if has_multi_targets:
+            # use an OrderedDict for legacy python compatibility
+            _items = [(dst_name, _create_dst_path(f_path, capt_subpath,
+                                                  _dst_pattern))
+                      for dst_name, _dst_pattern in dst_pattern.items()]
+            dst_paths = OrderedDict(_items)
+        else:
+            dst_paths = _create_dst_path(f_path, capt_subpath, dst_pattern)
 
-        for f_path, capt_subpath in match_generator:
-            # create the destination path(s)
-            if has_multi_targets:
-                # use an OrderedDict for legacy python compatibility
-                _items = [(dst_name, _create_dst_path(f_path, capt_subpath,
-                                                      _dst_pattern))
-                          for dst_name, _dst_pattern in dst_pattern.items()]
-                dst_paths = OrderedDict(_items)
-            else:
-                dst_paths = _create_dst_path(f_path, capt_subpath, dst_pattern)
+        # create the name
+        name = _create_dst_path(f_path, capt_subpath, names)
 
-            # finally create the container object and append
-            item = FileItem(src_path=f_path, dst_path=dst_paths,
-                            has_multi_targets=has_multi_targets)
-            res.append(item)
-
-    return res
+        # finally create the container object and append
+        yield FileItem(src_path=f_path, dst_path=dst_paths,
+                       has_multi_targets=has_multi_targets,
+                       name=name)
