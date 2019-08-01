31650b0d0cf14c8a0549d7768ad3569bdfdb34ee
Sylvain MARIE <sylvain.marie@se.com>
2019-08-01 15:39:00 +0200

    Updated tests according to latest changes

--- fprules/tests/test_basics.py
+++ fprules/tests/test_basics.py
@@ -10,45 +10,45 @@ except ImportError:
 
 def test_stem_only_simplest():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     res = file_pattern(resources / "foo/*.y*ml",  "./target/%.toto")
     expected = [
-        '%s/foo/xfile.yml -> target/xfile.toto' % resources.as_posix()
+        '[xfile] %s/foo/xfile.yml -> target/xfile.toto' % resources.as_posix()
     ]
     assert [str(r) for r in res] == expected
 
 
 def test_stem_with_multifolder_not_dst():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     res = file_pattern(resources / "foo/**/*.y*ml", "./target/%.toto")
     expected = [
-        '%s/foo/xfile.yml -> target/xfile.toto' % resources.as_posix(),
-        '%s/foo/bar/file3.yaml -> target/file3.toto' % resources.as_posix()
+        '[xfile] %s/foo/xfile.yml -> target/xfile.toto' % resources.as_posix(),
+        '[bar/file3] %s/foo/bar/file3.yaml -> target/file3.toto' % resources.as_posix()
     ]
     assert [str(r) for r in res] == expected
 
 
 def test_stem_with_multifolder_in_dst():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     res = file_pattern(str(resources) + "/foo/**/*.y*ml",  "./%%/target/%")
     expected = [
-        '%s/foo/xfile.yml -> target/xfile' % resources.as_posix(),
-        '%s/foo/bar/file3.yaml -> bar/target/file3' % resources.as_posix()
+        '[xfile] %s/foo/xfile.yml -> target/xfile' % resources.as_posix(),
+        '[bar/file3] %s/foo/bar/file3.yaml -> bar/target/file3' % resources.as_posix()
     ]
     assert [str(r) for r in res] == expected
 
 
 def test_stem_multifolder_multi_dst():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     # note: we use an ordered dict so that the test has reproducible order when on old python versions
@@ -56,35 +56,35 @@ def test_stem_multifolder_multi_dst():
                        dst_pattern=OrderedDict([('flat', "./target/%.toto"),
                                                 ('nested', "./%%/target2/%")]))
     expected = [
-        "%s/foo/xfile.yml -> {flat=target/xfile.toto, nested=target2/xfile}" % resources.as_posix(),
-        '%s/foo/bar/file3.yaml -> {flat=target/file3.toto, nested=bar/target2/file3}' % resources.as_posix()
+        '[xfile] %s/foo/xfile.yml -> {flat=target/xfile.toto, nested=target2/xfile}' % resources.as_posix(),
+        '[bar/file3] %s/foo/bar/file3.yaml -> {flat=target/file3.toto, nested=bar/target2/file3}' % resources.as_posix()
     ]
     assert [str(r) for r in res] == expected
 
 
 def test_nostem_nofolder():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     res = file_pattern(str(resources) + "/foo/", "./target/")
     expected = [
-        "%s/foo -> target" % resources.as_posix(),
+        "[foo] %s/foo -> target" % resources.as_posix(),
     ]
     assert [str(r) for r in res] == expected
 
 
 def test_nostem_folders():
     # locate the resources folder
-    resources = Path(__file__).parent / "resources"
+    resources = Path(__file__).parent / "resources" / "basics"
 
     # use the file_pattern and assert results
     res = file_pattern(str(resources) + "/foo/**/[!x]*", "./target/%%")
     expected = [
         # Warning: these first 2 are not intuitive but are actually correct answers.
-        "%s/foo/bar -> target" % resources.as_posix(),     # since /bar matches [!x]*
-        "%s/foo/barbar -> target" % resources.as_posix(),  # since /barbar matches [!x]*
-        "%s/foo/bar/file2 -> target/bar" % resources.as_posix(),
-        "%s/foo/bar/file3.yaml -> target/bar" % resources.as_posix(),
+        "[bar] %s/foo/bar -> target" % resources.as_posix(),     # since /bar matches [!x]*
+        "[barbar] %s/foo/barbar -> target" % resources.as_posix(),  # since /barbar matches [!x]*
+        "[bar/file2] %s/foo/bar/file2 -> target/bar" % resources.as_posix(),
+        "[bar/file3] %s/foo/bar/file3.yaml -> target/bar" % resources.as_posix(),
     ]
     assert [str(r) for r in res] == expected
