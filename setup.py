ab6f34ebd2c76c5c8298447c23addd5f0be0ce87
Sylvain MARIE <sylvain.marie@se.com>
2019-08-02 15:10:19 +0200

    Added `six` in the dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ here = path.abspath(path.dirname(__file__))
 # *************** Dependencies *********
 INSTALL_REQUIRES = ['pathlib2;python_version<"3.2"', 'makefun;python_version<"3.6"']
 DEPENDENCY_LINKS = []
-SETUP_REQUIRES = ['pytest-runner', 'setuptools_scm', 'pypandoc', 'pandoc']
+SETUP_REQUIRES = ['pytest-runner', 'setuptools_scm', 'pypandoc', 'pandoc', 'six']
 TESTS_REQUIRE = ['pytest', 'pytest-logging', 'pytest-cov',  #  'pytest-cases
                  'requests', 'tqdm', 'doit']
 EXTRAS_REQUIRE = {}
